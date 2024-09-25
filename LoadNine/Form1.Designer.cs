dcc499e190be93fe1480916aff3cae3749978f24
Song Hun Jeong <129823783+Bothpa@users.noreply.github.com>
2024-09-25 18:20:00 +0900

    Add files via upload

---
+++ LoadNine/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace LoadNine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(78, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "FHD(16:9)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "QHD(16:9)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 64);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(79, 16);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "UHD(16:9)";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(180, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(104, 132);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "해상도";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 87);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(83, 16);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "사용자설정";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("굴림", 11F);
+            this.textBox1.Location = new System.Drawing.Point(8, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 24);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 11F);
+            this.label1.Location = new System.Drawing.Point(6, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "디스코드 등록 이름";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(5, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Stauts :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(75, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Stop";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(5, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Count : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(75, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "0";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 219);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(285, 175);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(218, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Test";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("굴림", 12F);
+            this.label6.Location = new System.Drawing.Point(6, 148);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 16);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Error :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("굴림", 9F);
+            this.label8.Location = new System.Drawing.Point(77, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 12);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Not Error";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 402);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "죽음알림봇";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ LoadNine/Form1.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using OpenCvSharp;
+using System.IO;
+
+namespace LoadNine
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;    // 좌상단 X 좌표
+            public int Top;     // 좌상단 Y 좌표
+            public int Right;   // 우하단 X 좌표
+            public int Bottom;  // 우하단 Y 좌표
+        }
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowRect(IntPtr hWnd, out Rectangle rect);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+
+        internal static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern IntPtr GetDpiForWindow(IntPtr hWnd);
+
+        public bool searchIMG(Bitmap screen_img, Bitmap find_img, double sim)
+        {
+            try
+            {
+                using (Mat ScreenMat = OpenCvSharp.Extensions.BitmapConverter.ToMat(screen_img))
+                using (Mat FindMat = OpenCvSharp.Extensions.BitmapConverter.ToMat(find_img))
+                using (Mat res = ScreenMat.MatchTemplate(FindMat, TemplateMatchModes.CCoeffNormed))
+                {
+                    double minval, maxval = 0;
+                    OpenCvSharp.Point minloc, maxloc;
+                    Cv2.MinMaxLoc(res, out minval, out maxval, out minloc, out maxloc);
+
+                    if (maxval >= sim)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                label8.Text = "Game Load Error(SI)";
+                return false;
+            }
+        }
+
+        /*        public Bitmap GetTargetScreen(string targetWindowName)
+                {
+                    try
+                    {
+                        IntPtr findwindow = FindWindow(null, targetWindowName);
+                        if (findwindow != IntPtr.Zero)
+                        {
+                            Graphics Graphicsdata = Graphics.FromHwnd(findwindow);
+                            Rectangle rect;
+                            GetWindowRect(findwindow, out rect);
+                            float dpi = (float)GetDpiForWindow(findwindow) / 96f;
+                            int width = (int)(rect.Width * dpi);
+                            int height = (int)(rect.Height * dpi);
+                            Bitmap bmp = new Bitmap(width, height);
+
+                            using (Graphics g = Graphics.FromImage(bmp))
+                            {
+                                IntPtr hdc = g.GetHdc();
+                                PrintWindow(findwindow, hdc, 0x2);
+                                g.ReleaseHdc(hdc);
+                            }
+                            return bmp;
+                        }
+                        return new Bitmap(1, 1);
+                    }
+                    catch (Exception ex)
+                    {
+                        label8.Text = "Game Load Error(GTS)";
+                        MessageBox.Show(ex.ToString());
+                        return null;
+                    }
+                }*/
+
+        public Bitmap GetTargetScreen(string targetWindowName)
+        {
+            Bitmap bmp = null;
+            Graphics g = null;
+
+            try
+            {
+                IntPtr findwindow = FindWindow(null, targetWindowName);
+/*                // 핸들 유효성 확인
+                if (findwindow == IntPtr.Zero)
+                {
+                    label8.Text = "Game window not found.";
+                    return null;
+                }
+
+                // 게임 창이 보이는지 확인
+                if (!IsWindowVisible(findwindow))
+                {
+                    label8.Text = "Game window is not visible.";
+                    return null;
+                }*/
+
+                using (Graphics Graphicsdata = Graphics.FromHwnd(findwindow))
+                {
+                    Rectangle rect;
+                    GetWindowRect(findwindow, out rect);
+                    float dpi = (float)GetDpiForWindow(findwindow) / 96f;
+                    int width = (int)(rect.Width * dpi);
+                    int height = (int)(rect.Height * dpi);
+
+                    /*if (width <= 0 || height <= 0)
+                    {
+                        label8.Text = "Invalid window dimensions.";
+                        return null;
+                    }*/
+
+                    bmp = new Bitmap(width, height);
+
+                    g = Graphics.FromImage(bmp);
+                    IntPtr hdc = g.GetHdc();
+                    if (!PrintWindow(findwindow, hdc, 0x2))
+                    {
+                        throw new Exception("PrintWindow failed.");
+                    }
+                    g.ReleaseHdc(hdc);
+                }
+
+                return bmp;
+            }
+            catch (Exception ex)
+            {
+                label8.Text = "Game Load Error(GTS)";
+                timer1.Stop();
+                label3.Text = "Stop...";
+                count = 0;
+                button1.Enabled = true; // 버튼 다시 활성화
+                MessageBox.Show("게임 인식 에러!!! \n 게임이 종료되었다면 다시 켜주시고 게임 최소화를 풀어주세요.","경고");
+                
+                return null;
+            }
+            finally
+            {
+                if (g != null)
+                {
+                    g.Dispose();
+                }
+                if (bmp != null && bmp.Width == 1 && bmp.Height == 1)
+                {
+                    bmp.Dispose();
+                }
+            }
+        }
+
+
+
+
+
+
+
+        public bool SimpleCheck(string Path, string targetWindowName)
+        {
+            Bitmap screen = null;
+            Bitmap findimg = null;
+            try
+            {
+                screen = GetTargetScreen(targetWindowName);
+                if (screen == null)
+                {
+                    return false;
+                }
+                findimg = new Bitmap(Path);
+                label8.Text = "Not Error";
+                return searchIMG(screen, findimg, 0.5);
+            }
+            catch (Exception)
+            {
+                label8.Text = "Game Load Error(SC)";
+                return false;
+            }
+            finally
+            {
+                if(screen != null)
+                {
+                    screen.Dispose();
+                }
+                if(findimg != null)
+                {
+                    findimg.Dispose();
+                }
+            }
+        }
+
+        private async Task<string> SendPostRequestAsync(object data)
+        {
+            try
+            {
+                // 데이터 객체를 JSON 문자열로 직렬화 (Newtonsoft.Json 사용)
+                var json = JsonConvert.SerializeObject(data);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                // POST 요청 보내기
+                HttpResponseMessage response = await client.PostAsync("http://jungsonghun.iptime.org:9999", content);
+
+                // 응답 상태 코드 확인
+                if (response.IsSuccessStatusCode)
+                {
+                    // 응답 본문 읽기
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    // 오류 메시지 반환
+                    return $"Error: {response.StatusCode}";
+                }
+            }
+            catch (Exception ex)
+            {
+                timer1.Stop();
+                label3.Text = "Stop...";
+                count = 0;
+                button1.Enabled = true; // 버튼 다시 활성화
+                MessageBox.Show("서버 오류");
+                return $"Exception: {ex.Message}";
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string name = textBox1.Text;
+                if (name == "")
+                {
+                    MessageBox.Show("디스코드에 등록한 이름을 입력해주세요.", "알림 메세지");
+                    return;
+                }
+
+                button1.Enabled = false; // 버튼 비활성화
+                timer1.Start();
+                count += 1;
+                label5.Text = count.ToString();
+                label3.Text = "Running..";               
+
+                if (radioButton1.Checked)
+                {
+                    if (SimpleCheck("findtoimage\\FHD.PNG", "LORDNINE"))
+                    {
+                        await SendPostRequestAsync(new { name = name });
+                        button2_Click(sender, e);
+                    }
+                }
+                else if (radioButton2.Checked)
+                {
+                    if (SimpleCheck("findtoimage\\QHD.PNG", "LORDNINE"))
+                    {
+                        await SendPostRequestAsync(new { name = name });
+                        button2_Click(sender, e);
+                    }
+                }
+                else if (radioButton3.Checked)
+                {
+                    button2_Click(sender, e);
+                    MessageBox.Show("개발중인 해상도 입니다.");
+                }
+                else if (radioButton4.Checked)
+                {
+                    if (File.Exists("findtoimage\\USER.PNG"))
+                    {
+                        if (SimpleCheck("findtoimage\\USER.PNG", "LORDNINE"))
+                        {
+                            await SendPostRequestAsync(new { name = name });
+                            button2_Click(sender, e);
+                        }
+                    }
+                    else
+                    {
+                        button2_Click(sender, e);
+                        MessageBox.Show("사용자 지정 이미지를 먼저 넣어주세요.", "LORDNINE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                                   
+                }
+                else
+                {
+                    button2_Click(sender, e);
+                    MessageBox.Show("해상도를 선택해주세요.");
+                }
+            }
+            catch (Exception)
+            {
+                button2_Click(sender, e);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                timer1.Stop();
+                label3.Text = "Stop...";
+                count = 0;
+                label8.Text = "Not Error";
+                button1.Enabled = true; // 버튼 다시 활성화
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                button1_Click(sender, e);   
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap screenImage = null;
+            try
+            {
+                if(screenImage != null)
+                {
+                    screenImage.Dispose();
+                }
+                screenImage = GetTargetScreen("LORDNINE");
+
+                if (screenImage != null)
+                {
+                    if (pictureBox2.Image != null)
+                    {
+                        pictureBox2.Image.Dispose();
+                    }
+                    pictureBox2.Image = screenImage;
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    label8.Text = "Not Error";
+                }
+            }
+            catch (Exception)
+            {
+                label8.Text = "Game Load Error(B3)";
+            }
+        }
+
+
+
+        //private bool CheckProcess(string Name)
+        //{
+        //    Process process;
+        //    try
+        //    {
+        //        process = Process.GetProcessesByName(Name)[0];
+        //    }
+        //    catch
+        //    {
+        //        return false;
+        //    }
+
+        //    if (process == null)
+        //    {
+
+        //        return false;
+        //    }
+        //    else { return true; }
+        //}
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+        //    if (!CheckProcess("LORDNINE.exe"))
+        //    {
+        //        MessageBox.Show("게임을 먼저 실행시켜주세요.", "LORDNINE", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        //        Environment.Exit(0);
+        //    }
+        //}
+    }
+}
